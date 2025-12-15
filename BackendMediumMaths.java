c91cd250fba1a74ebaa8bf0c3713154304180ec1
MickyDa1 <mic25567@gmail.com>
2025-12-15 21:59:22 +0000

    CORRECTED SOME ERRORS

--- BackendMediumMaths.java
+++ BackendMediumMaths.java
@@ -1,32 +1,31 @@
 public class BackendMediumMaths{
 
     public static void keneticEnergy(int mass, int Velocity){
-        int intTotal = 0.5 x mass x (Velocity x Velocity);
-        convertingTotal(intTotal);
+        int intTotal = (int) 0.5 * mass * (Velocity * Velocity);
+        String total = convertingTotal(intTotal);
         base.result("Kenetic Energy", total);
     }
 
     public static void weight(int mass, int gfs){ //gfs = Gravitational Field Strength
-        int intTotal = mass x gfs;
-        convertingTotal(intTotal);
+        int intTotal = mass * gfs;
+        String total = convertingTotal(intTotal);
         base.result("Weight", total);
     }
     
     public static void hydrostaticPressure(int height, int density, int gfs){ //Pressure of a liquid at rest.
-        int intTotal = height x density x gfs;
-        convertingTotal(intTotal);
+        int intTotal = height * density * gfs;
+        String total = convertingTotal(intTotal);
         base.result("Hydrostatic Pressure", total);
     }
 
     public static void elasticPotentialEnergy(int springConstant, int Extenstion){
-        int intTotal = 0.5 x sprintConstant x (Extenstion x Extenstion);
-        convertingTotal(intTtoal);
+        int intTotal = (int) (0.5 * springConstant * (Extenstion * Extenstion));
+        String total = convertingTotal(intTotal);
         base.result("Elastic Potential Energy", total);
     }
 
-    public static convertingTotal(int intTotal){
-        String total = String.valueOf(intTotal);
-        return total;
+    public static String convertingTotal(int intTotal){
+        return String.valueOf(intTotal);
     }
 
 }
