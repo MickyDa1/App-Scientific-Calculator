f58d10508cf4b7d5b56010f13285fc90144ba968
MickyDa1 <mic25567@gmail.com>
2025-12-28 23:32:28 +0000

    was able to rescue from previous failed merge

---
+++ ElasticPotentialEnergy.java
@@ -0,0 +1,53 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class ElasticPotentialEnergy {
+    public static void main() {
+        base.setUp("Elastic Potential Energy");
+
+        JLabel headingLabel = new JLabel("Please Fill In The Following Boxes:");
+        headingLabel.setBounds(86, 65, 300, 25);
+        headingLabel.setForeground(Color.black);
+        base.frame.add(headingLabel);
+
+		JLabel inputALabel = new JLabel("Number for Spring Constant(N/m)): ");
+		inputALabel.setBounds(50, 125, 200, 25);
+		inputALabel.setForeground(Color.black);
+		base.frame.add(inputALabel);
+		
+		JTextField springConstant = new JTextField();
+		springConstant.setBounds(170, 125, 150, 25);
+	    base.frame.add(springConstant);
+
+        JLabel inputBLabel = new JLabel("Number for Extension(m): ");
+		inputBLabel.setBounds(50, 200, 200, 25);
+		inputBLabel.setForeground(Color.black);
+		base.frame.add(inputBLabel);
+		
+		JTextField Extenstion = new JTextField(); 
+		Extenstion.setBounds(170, 200, 150, 25);
+	    base.frame.add(Extenstion);
+        //Calls Image
+
+        //Calls Appropriate Maths Function!!
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBounds(140, 300, 115, 25); //place x, place y, 115, 25  
+        base.frame.add(submitButton);
+        submitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double springConstantValue = Double.parseDouble(springConstant.getText());
+                    double ExtenstionValue = Double.parseDouble(Extenstion.getText());
+
+                    BackendMediumMaths.elasticPotentialEnergy(springConstantValue, ExtenstionValue);
+                } catch (IllegalArgumentException b) {
+                    //base.mistake("ERROR", "SOMETHING WENT WRONG, PLEASE TRY AGAIN IF THE PROBLEM CONTINUES, PLEASE CHECK THE BOXES");
+                }
+            }
+        });
+    }
+}
--- Main.java
+++ Main.java
@@ -13,29 +13,14 @@ public class Main {
         mainMenuLabel.setBounds(86, 65, 300, 25);
         mainMenuLabel.setForeground(Color.black);
         base.frame.add(mainMenuLabel);
-        
-        JButton simpleSumButton = new JButton("Simple Sums");
-        simpleSumButton.setBounds(140, 100, 115, 25); //place x, place y, height, width
-        base.frame.add(simpleSumButton);
-<<<<<<< HEAD
 
-
-=======
-        simpleSumButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                base.frame.setVisible(false);
-				        SimpleMenu.Main();
-            }
-        });
-  
->>>>>>> dd879edb07a14520136479a4a4c08dc69aee2ffa
         JButton MediumSumButton = new JButton("Medium Sums");
         MediumSumButton.setBounds(140, 120, 115, 25);
         base.frame.add(MediumSumButton);
         MediumSumButton.addActionListener(new ActionListener() {
            public void actionPerformed(ActionEvent e) {
                 base.frame.setVisible(false);
-				        MedioumMenu.Main();
+				        MediumMenu.Main();
             }
         });
       
